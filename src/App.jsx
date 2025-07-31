3246cebd9d6fa3ba657ea03bb0b742019b0a3694
Zain <zainabad27@gmail.com>
2025-07-31 10:11:59 +0500

    initial commits.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // let counter = 5;
+  let [counter, setCounter] = useState(10);
+
+  const addvalue = () => {
+    if(counter===10){
+      setCounter(10);
+      return;
+    }
+    setCounter(counter + 1);
+  }
+
+  const subtractvalue = () => {
+    if (counter === 0) {
+      setCounter(0);
+      return;
+    }
+    setCounter(counter - 1);
+  }
+  const boostvalue = () => {
+    setCounter(counter + 10000);
+  }
+
+
+  return (
+    <>
+
+      <h1>First Project</h1>
+      <p>Current Counter Value: {counter}</p>
+      <button onClick={addvalue}>+1</button>
+      <button onClick={subtractvalue}>-1</button>
+      <button onClick={boostvalue}>Boost Value</button>
+    </>
+  )
+}
+
+export default App
